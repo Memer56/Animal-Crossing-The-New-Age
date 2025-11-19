8dd9c2f166025e6637ffc5345bda35c0c1417a36
Memer56 <96455752+Memer56@users.noreply.github.com>
2025-11-19 02:52:39 +0000

    Created new item data type

---
+++ Inventory/Item/item_data_positioning.gd
@@ -0,0 +1,5 @@
+extends ItemData
+class_name ItemDataPositioning
+
+# Tom sets what type this item is, this item is created via code i.e talk to tom, tome gives this item
+@export_enum("Furniture or Plant", "Building") var item_type
