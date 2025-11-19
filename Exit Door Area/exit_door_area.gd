49b8c00fa1d4d416d451abc48de232b39b8e044a
Memer56 <96455752+Memer56@users.noreply.github.com>
2025-11-19 02:51:03 +0000

    Add files via upload

---
+++ Exit Door Area/exit_door_area.gd
@@ -0,0 +1,16 @@
+extends Area3D
+
+var allow_player_to_exit : bool = false
+var set_speed_to_zero : bool = false
+
+func _on_body_entered(body: Node3D) -> void:
+	if body.get_collision_layer() == 1 and allow_player_to_exit:
+		body.doorway_events_can_trigger = true
+		body.should_force_rotation_for_entry = false
+		body.state = body.ENTER_DOOR
+		body.toggle_collisions(2, false)
+		EventBus.trigger_building_exit_event = true
+
+
+func _on_body_exited(_body: Node3D) -> void:
+	allow_player_to_exit = true
