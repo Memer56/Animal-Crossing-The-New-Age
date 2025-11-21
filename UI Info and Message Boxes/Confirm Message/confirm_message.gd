352ff084cfcf099efd484cc3bb21e3f0676646b3
Memer56 <96455752+Memer56@users.noreply.github.com>
2025-11-21 04:38:17 +0000

    Updated confirm message logic

--- UI Info and Message Boxes/Confirm Message/confirm_message.gd
+++ UI Info and Message Boxes/Confirm Message/confirm_message.gd
@@ -1,5 +1,7 @@
 extends Control
 
+signal temp_confirm_signal()
+
 @export var text : String
 @export var is_for_placing_object : bool = false
 @onready var label: Label = $Panel/Label
@@ -12,6 +14,8 @@ func _on_confirm_pressed() -> void:
 	if is_for_placing_object:
 		BuildManager.spawn_object()
 		queue_free()
+	else:
+		temp_confirm_signal.emit()
 
 
 func _on_cancel_pressed() -> void:
