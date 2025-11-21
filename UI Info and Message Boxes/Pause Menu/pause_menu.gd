328d3c6c7033ade0d8bd4704127ddf2857afd786
Memer56 <96455752+Memer56@users.noreply.github.com>
2025-11-21 04:39:05 +0000

    Overhauled pause menu

--- UI Info and Message Boxes/Pause Menu/pause_menu.gd
+++ UI Info and Message Boxes/Pause Menu/pause_menu.gd
@@ -1,9 +1,5 @@
 extends Control
 
-
-func _on_button_pressed() -> void:
-	EventBus.save_game_data.emit()
-
 func _input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("Pause"):
 		toggle_game_pause()
@@ -20,3 +16,27 @@ func toggle_self():
 		visible = false
 	else:
 		visible = true
+
+
+func _on_resume_pressed() -> void:
+	toggle_game_pause()
+	toggle_self()
+
+
+func _on_settings_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_save_pressed() -> void:
+	EventBus.save_game_data.emit()
+	toggle_self()
+	EventBus.display_speech_bubble.emit(["Game saved [color=green]successfully[/color]"], "Yayy")
+	toggle_game_pause()
+
+
+func _on_save_and_quit_pressed() -> void:
+	EventBus.is_returning_to_main_menu = true
+	EventBus.save_game_data.emit()
+	FadeCanvasLayer.trigger_scene_change_fade(true)
+	await get_tree().create_timer(0.6).timeout
+	get_tree().change_scene_to_file.bind("res://UI Info and Message Boxes/Main Menu/main_menu.tscn").call_deferred()
