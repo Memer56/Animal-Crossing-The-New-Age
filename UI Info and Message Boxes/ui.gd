b99a204efc4a8a77734cb613810401a22ee539b9
Memer56 <96455752+Memer56@users.noreply.github.com>
2026-04-23 02:22:41 +0100

    Fixed issues for pause menu not working

--- UI Info and Message Boxes/ui.gd
+++ UI Info and Message Boxes/ui.gd
@@ -1,6 +1,9 @@
 extends CanvasLayer
 
 const CONFIRM_MESSAGE = preload("uid://btn78ebu7dnjd")
+const CHEAT_MENU_UI = preload("uid://ww7phe1tiqck")
+
+@onready var inventory_interface: Control = $InventoryInterface
 
 func _ready() -> void:
 	EventBus.trigger_confirm_message.connect(spawn_confirm_message_box)
@@ -10,3 +13,14 @@ func spawn_confirm_message_box(main_text : String):
 	msg_box.text = main_text
 	msg_box.is_for_placing_object = true
 	add_child(msg_box)
+
+func _input(_event: InputEvent) -> void:
+	if Input.is_action_just_pressed("ToggleCheatMenu"):
+		if !CheatManager.cheat_menu_displayed:
+			var cheat_menu = CHEAT_MENU_UI.instantiate()
+			# This allows the PauseMenu to be clickable due to scenetree order
+			inventory_interface.add_sibling(cheat_menu)
+			CheatManager.cheat_menu_displayed = true
+		else:
+			CheatManager.remove_ui.emit()
+			CheatManager.cheat_menu_displayed = false
