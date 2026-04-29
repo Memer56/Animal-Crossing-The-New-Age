fb838dd6d68f17e72b52249594aa2b55b9f03279
Memer56 <96455752+Memer56@users.noreply.github.com>
2026-04-29 03:20:48 +0100

    Updated these

--- Cheat Manager/cheat_menu_ui.gd
+++ Cheat Manager/cheat_menu_ui.gd
@@ -4,12 +4,13 @@ const CRAFTING_OPTION_DISPLAY_BUTTON = preload("uid://c5afvfdm0pit6")
 
 @export var options_list_buttons : Array[Button]
 @onready var general_cheats_container: VBoxContainer = $BG/GeneralCheatsContainer
-@onready var give_items_container: VBoxContainer = $GiveItemsContainer
-@onready var search_bar: LineEdit = $GiveItemsContainer/SearchBar
+@onready var give_items_container: VBoxContainer = $BG/GiveItemsContainer
+@onready var search_bar: LineEdit = $BG/GiveItemsContainer/SearchBar
 @onready var general_cheats: Button = $BG/OptionsBG/OptionsList/GeneralCheats
-@onready var selected_item_texture: TextureRect = $GiveItemsContainer/HBoxContainer/Panel/SelectedItemTexture
+@onready var selected_item_texture: TextureRect = $BG/GiveItemsContainer/HBoxContainer/Panel/SelectedItemTexture
 
 var mouse_is_on_search_bar : bool = false
+var current_slot_data : SlotData
 
 func _ready() -> void:
 	CheatManager.remove_ui.connect(remove_self)
@@ -47,6 +48,10 @@ func _on_search_bar_mouse_exited() -> void:
 	mouse_is_on_search_bar = false
 
 
+func _on_search_bar_text_submitted(new_text: String) -> void:
+	pass # Replace with function body.
+
+
 func _on_gui_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT or event.button_index == MOUSE_BUTTON_RIGHT:
@@ -56,4 +61,14 @@ func _on_gui_input(event: InputEvent) -> void:
 
 func _on_crafting_option_display_button_send_data(item_texture: AtlasTexture, slot_data: SlotData) -> void:
 	selected_item_texture.texture = item_texture
-	print("Send data")
+	current_slot_data = slot_data
+
+
+func _on_give_item_button_pressed() -> void:
+	if current_slot_data:
+		if EventBus.player.hotbar_inventory_data.add_to_inventory(current_slot_data):
+			return
+		elif EventBus.player.inventory_data.add_to_inventory(current_slot_data):
+			return
+		else:
+			EventBus.display_speech_bubble.emit(["Inventory is [color=red]full[/color]."], "Info", [], null)
