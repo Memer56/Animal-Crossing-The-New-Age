72476aadb22a8c9dad95343442ad244690c48707
Memer56 <96455752+Memer56@users.noreply.github.com>
2026-05-14 03:50:38 +0100

    Updated to match villager names

--- NPC's/npc_dialogue.gd
+++ NPC's/npc_dialogue.gd
@@ -8,11 +8,11 @@ class_name NpcDialogue
 
 @export var tom_answer_button_options_1 : Array = ["what should I do?", "About my home...", "I'm good"]
 
-@export var bunny_dialogue : Array = [
-	[["Hello, I'm [color=blue]<Insert fucking name>[/color], who are you?"], "Bunny", [], null]
+@export var bunnie_dialogue : Array = [
+	[["Hello, I'm [color=blue]<Insert fucking name>[/color], who are you?"], "Bunnie", [], null]
 ]
-@export var cat_dialogue : Array = [
-	[["Why hello there! I'm a [color=purple]CAT[/color]! You don't look like a cat", "I do need a potato...", "Can I take [color=blue]100,000[/color] bells?"], "Cat", [], null]
+@export var bob_dialogue : Array = [
+	[["Why hello there! I'm a [color=purple]CAT[/color]! You don't look like a cat", "I do need a potato...", "Can I take [color=blue]100,000[/color] bells?"], "Bob", [], null]
 ]
 
 @export var timmy_answer_button_options : Array = ["I'll take it!", "No thanks"]
@@ -26,9 +26,9 @@ func get_correct_dialogue(npc_name : String, dialogue_index : int) -> Array:
 			pass
 		"Tommy":
 			pass
-		"Bunny":
-			dialogue_array = bunny_dialogue[dialogue_index]
-		"Cat":
-			dialogue_array = cat_dialogue[dialogue_index]
+		"Bunnie":
+			dialogue_array = bunnie_dialogue[dialogue_index]
+		"Bob":
+			dialogue_array = bob_dialogue[dialogue_index]
 	
 	return dialogue_array
