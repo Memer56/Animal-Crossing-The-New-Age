585b00720583f19d110ffc610e0ce99fc65755ba
Memer56 <96455752+Memer56@users.noreply.github.com>
2026-05-14 03:54:42 +0100

    Clicking New Save redirects to selection screen and fixed infinite loading bug

--- UI Info and Message Boxes/Load Screen/load_screen.gd
+++ UI Info and Message Boxes/Load Screen/load_screen.gd
@@ -2,25 +2,31 @@ extends Control
 
 @onready var loading_bar: ProgressBar = $BackgroundBar/LoadingBar
 
-var next_scene = "res://Main World/main.tscn"
+var next_scene
 var last_progress = 0.0
 
 func _ready() -> void:
+	if EventBus.game_is_new_save:
+		next_scene = "res://Player Selection Screen/player_selection_screen.tscn"
+	else:
+		next_scene = "res://Main World/main.tscn"
+	
 	ResourceLoader.load_threaded_request(next_scene, "")
 
 
 func _process(delta: float) -> void:
-	var progress = []
-	var loaded_status = ResourceLoader.load_threaded_get_status(next_scene, progress)
-	var new_progress = progress[0] * 100.0
-	
-	if new_progress > last_progress:
-		last_progress = new_progress
+	if next_scene:
+		var progress = []
+		var loaded_status = ResourceLoader.load_threaded_get_status(next_scene, progress)
+		var new_progress = progress[0] * 100.0
+		
+		if new_progress > last_progress:
+			last_progress = new_progress
 
-	loading_bar.value = lerp(loading_bar.value, last_progress, delta * 5)
-	
-	if loaded_status == ResourceLoader.ThreadLoadStatus.THREAD_LOAD_LOADED:
-		loading_bar.value = 100.0
-		var packed_next_scene = ResourceLoader.load_threaded_get(next_scene)
-		await get_tree().create_timer(0.5).timeout
-		get_tree().change_scene_to_packed(packed_next_scene)
+		loading_bar.value = lerp(loading_bar.value, last_progress, delta * 5)
+		
+		if loaded_status == ResourceLoader.ThreadLoadStatus.THREAD_LOAD_LOADED:
+			loading_bar.value = 100.0
+			var packed_next_scene = ResourceLoader.load_threaded_get(next_scene)
+			await get_tree().create_timer(0.5).timeout
+			get_tree().change_scene_to_packed(packed_next_scene)
